082bfad101e56a5a768783d50a88804528b5009d
KJam <kaytchjam@gmail.com>
2025-10-17 00:51:26 -0500

    made m_low and m_high public, and changed the tabbing

--- src/intrange.hpp
+++ src/intrange.hpp
@@ -3,44 +3,44 @@
 
 /** Range representing the interval [low, high). Note that high is not included in the range */
 class IntRange {
-    private:
-        int m_low;
+public:
+    const int m_low;
+    const int m_high;
+
+    IntRange();
+    IntRange(int high);
+    IntRange(int low, int high);
+    IntRange(const IntRange& ir);
+    ~IntRange();
+
+    int low() const;
+    int high() const;
+
+    struct IntRangeIterator {
+        // Iterator Traits
+        using value_type = int;
+        using difference_type = int;
+        using pointer_type = void;
+        using reference_type = int; 
+        using iterator_category = std::input_iterator_tag;
+
+        int m_at;
         int m_high;
-    public:
-        IntRange();
-        IntRange(int high);
-        IntRange(int low, int high);
-        IntRange(const IntRange& ir);
-        ~IntRange();
+        IntRangeIterator(int start, int high);
 
-        int low() const;
-        int high() const;
+        value_type operator*() const;
+        IntRangeIterator& operator++();
+        IntRangeIterator operator++(int);
 
-        struct IntRangeIterator {
-            // Iterator Traits
-            using value_type = int;
-            using difference_type = int;
-            using pointer_type = void;
-            using reference_type = int; 
-            using iterator_category = std::input_iterator_tag;
+        bool operator==(const IntRangeIterator& other) const;
+        bool operator!=(const IntRangeIterator& other) const;
+    };
 
-            int m_at;
-            int m_high;
-            IntRangeIterator(int start, int high);
+    typedef IntRangeIterator iterator;
 
-            value_type operator*() const;
-            IntRangeIterator& operator++();
-            IntRangeIterator operator++(int);
-
-            bool operator==(const IntRangeIterator& other) const;
-            bool operator!=(const IntRangeIterator& other) const;
-        };
-
-        typedef IntRangeIterator iterator;
-
-        iterator begin() const;
-        iterator end() const;
-        
-        bool contains(int i) const;
-        bool operator==(const IntRange& other)  const;
+    iterator begin() const;
+    iterator end() const;
+    
+    bool contains(int i) const;
+    bool operator==(const IntRange& other)  const;
 };
