6eb441e27dd0c21b7de6f1fa2c81492c4049daaa
KJam <kaytchjam@gmail.com>
2025-10-17 01:47:02 -0500

    removed implementations and moved them to isosurface.cpp

--- src/isosurface.hpp
+++ src/isosurface.hpp
@@ -5,13 +5,6 @@
 #include <vector>
 #include <iostream>
 
-static unsigned int my_pow_int(const unsigned int base, const unsigned int exp) {
-    if (base == 0 || base == 1) return base;
-    unsigned int acc = 1;
-    for (int j = 0; j < exp; j++) { acc *= base; }
-    return acc;
-}
-
 namespace mball {
     /** Represents a cube corner point in an IsoSurface. */
     struct IsoPoint {
@@ -40,16 +33,12 @@ namespace mball {
      * on position 'center'. */
     class IsoSurface {
     private:
-        float side_length;
-        unsigned int partitions;
-        glm::vec3 center_position;
-        std::vector<IsoPoint> isopoints;
+        float m_side_length;
+        uint32_t m_partitions;
+        glm::vec3 m_center_position;
+        std::vector<IsoPoint> m_isopoints;
 
-        IsoSurface(const glm::vec3& center, float length, unsigned int partitions) 
-            : center_position(center), side_length(length), 
-            partitions(partitions), isopoints() {
-            isopoints.reserve(my_pow_int(partitions + 1, 3));
-        }
+        IsoSurface(const glm::vec3& center, float length, uint32_t partitions);
     public:
         ~IsoSurface() = default;
     
@@ -70,48 +59,21 @@ namespace mball {
          * must be an EVEN positive integer.
          * 
          * If partitions is odd, `new_partitions = partitions - 1` */
-        static IsoSurface construct(const glm::vec3& center, float side_length, unsigned int partitions) {
-            assert(partitions > 1);
-            
-            partitions -= (partitions & 0x1) == 1; // makes partition odd if even
-            const int axis_indices = partitions + 1;
-            const IndexDim mid_idx = IndexDim(axis_indices) / 2;
-
-            IsoSurface surface = IsoSurface(center, side_length, partitions);
-
-            glm::vec3 offset;
-            glm::vec3 ratios;
-            const float half_indices = (float) mid_idx.x;
-            for (const IndexDim& p_idx : FieldRange(0, axis_indices)) {
-                offset = glm::vec3(p_idx - mid_idx);
-                ratios = offset / half_indices;
-                surface.isopoints.push_back( IsoPoint{ ratios * side_length, 0.0 } );
-            }
-
-            return surface;
-        }
+        static IsoSurface construct(const glm::vec3& center, const float side_length, uint32_t partitions);
 
         /** Returns an IndexCompactor that uses
          * the shape of the IsoSurface as a context */
-        IndexCompactor compactor() const {
-            return IndexCompactor(partitions + 1);
-        }
+        IndexCompactor compactor() const;
 
         /** Returns the number of indices (or points) within the IsoSurface */
-        size_t indices() const {
-            return isopoints.size();
-        }
+        size_t indices() const;
 
         /** Returns the shape (indices per axis) of this IsoSurface 
          * as an IndexDim */
-        IndexDim shape() const {
-            return IndexDim(partitions + 1);
-        }
+        IndexDim shape() const;
 
         /** Returns the side length of this IsoSurface */
-        float length() const {
-            return this->side_length;
-        }
+        float length() const;
 
         /** For debugging */
         void _print() const {
