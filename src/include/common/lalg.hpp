61713715b930379031add8c7a1838716ee54a0de
KJam <kaytchjam@gmail.com>
2026-01-12 23:38:48 -0600

    more functions! clamp, lerp, equal_eps

--- src/include/common/lalg.hpp
+++ src/include/common/lalg.hpp
@@ -18,7 +18,6 @@ namespace mbl {
          * of Metaball Engine's dependencies I'll be using glm pretty liberally. I just don't want people
          * using this code to download additional things or fidget with includes if they just want
          * to use the Metaball Engine alone. */
-        // namespace lalg {
         namespace lalg {
             template<typename V>
             struct VecTraits;
@@ -47,6 +46,7 @@ namespace mbl {
                 requires (const V& v) { v[0]; 
             };
 
+            /** Simple representation of a Algebraic Vector over some type `T` with dimension `N`. */
             template <typename T, size_t N>
             struct Vec {
                 T data[N];
@@ -84,10 +84,10 @@ namespace mbl {
                 Vec(T a,T b,T c) : x(a),y(b),z(c) {}
                 Vec(const T& t) : x(t), y(t), z(t) {}
 
-                template<typename Expr, typename = HasSubscript<Expr>>
+                template<VecLike Expr>
                 Vec(const Expr& e) : x((T)e[0]), y((T)e[1]), z((T)e[2]) {}
 
-                template <typename Expr, typename = HasSubscript<Expr>>
+                template <VecLike Expr>
                 Vec& operator+=(const Expr& rhs) {
                     static_assert(VecSize<Expr> == 3);
                     x += rhs[0];
@@ -96,7 +96,7 @@ namespace mbl {
                     return *this;
                 }
 
-                template <typename Expr, typename = HasSubscript<Expr>>
+                template <VecLike Expr>
                 Vec& operator-=(const Expr& rhs) {
                     static_assert(VecSize<Expr> == 3);
                     x -= rhs[0];
@@ -126,6 +126,7 @@ namespace mbl {
                 static constexpr size_t size = N;
             };
             
+            /** Vector expression that sums two VecLikes. */
             template<typename L, typename R>
             struct SumVec {
                 const L& a;
@@ -137,6 +138,7 @@ namespace mbl {
                 value_type operator[](size_t i) const { return a[i] + b[i]; }
             };
             
+            /** Vector expression that subtracts two VecLikes. */
             template<typename L, typename R>
             struct SubVec {
                 const L& a;
@@ -148,6 +150,7 @@ namespace mbl {
                 value_type operator[](size_t i) const { return a[i] - b[i]; }
             };
             
+            /** Vector expression that scales a VecLike */
             template<typename V, typename S>
             struct ScaleVec {
                 const V& v;
@@ -159,6 +162,7 @@ namespace mbl {
                 value_type operator[](size_t i) const { return v[i] * s; }
             };
             
+            /** Vector expression that divides a VecLike */
             template<typename V, typename S>
             struct DivVec {
                 const V& v;
@@ -170,6 +174,7 @@ namespace mbl {
                 value_type operator[](size_t i) const { return v[i] / s; }
             };
 
+            /** Vector expression that combines two VecLikes via some BinaryMap function. */
             template<typename L, typename R, typename M>
             struct BinaryMapVec {
                 const L& a;
@@ -182,6 +187,7 @@ namespace mbl {
                 value_type operator[](size_t i) const { return m(a[i],b[i]); }
             };
 
+            /** Vector expression that maps a function over some VecLike. */
             template<typename V, typename M>
             struct MapVec {
                 const V& v;
@@ -232,16 +238,12 @@ namespace mbl {
             template<typename L, typename R>
             SumVec<L,R> operator+(const L& l, const R& r) {
                 static_assert(VecSize<L> == VecSize<R>, "lalg::operator+(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
-
                 return SumVec<L,R>{l,r};
             }
             
             template<typename L, typename R>
             SubVec<L,R> operator-(const L& l, const R& r) {
-                static_assert(VecSize<L> == VecSize<R>,
-                    "lalg::operator-(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size."
-                );
-
+                static_assert(VecSize<L> == VecSize<R>, "lalg::operator-(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
                 return SubVec<L,R>{l,r};
             }
 
@@ -262,17 +264,13 @@ namespace mbl {
             
             template <VecLike L, VecLike R, typename BinaryMap> requires std::is_invocable_v<BinaryMap,VecValue<L>,VecValue<R>>
             BinaryMapVec<L,R,BinaryMap> mapply(const L& l, const R& r, const BinaryMap& b) {
-                static_assert(VecSize<L> == VecSize<R>,
-                    "lalg::mapply(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size."
-                );
-                
+                static_assert(VecSize<L> == VecSize<R>, "lalg::mapply(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
                 return BinaryMapVec<L,R,BinaryMap>{l,r,b};
             }
 
             template <VecLike L, VecLike R>
             bool operator==(const L& l, const R& r) {
                 static_assert(VecSize<L> == VecSize<R>, "lalg::operator==(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
-
                 for (int i = 0; i < VecSize<L>; i++) {
                     if (l[i] != r[i]) {
                         return false;
@@ -283,15 +281,18 @@ namespace mbl {
             
             template <VecLike L, VecLike R>
             bool operator!=(const L& l, const R& r) {
+                static_assert(VecSize<L> == VecSize<R>, "lalg::operator!=(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
                 return !(l == r);
             }
 
+            /** Apply some function `Map m` over the elements in `VecLike v`. */
             template <VecLike V, typename Map> requires std::is_invocable_v<Map, VecValue<V>>
             MapVec<V,Map> map(const V& v, const Map& m) {
                 return MapVec<V,Map>{v,m};
             }
 
-            template <VecLike L, VecLike R>
+            /** Return the dot product between `VecLike v` and `VecLike w`  */
+            template <VecLike L, VecLike R> 
             VecValue<L> dot(const L& v, const R& w) {
                 static_assert(VecSize<L> == VecSize<R>, "lalg::cosine_of(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
                 static_assert(std::is_same<VecValue<L>, VecValue<R>>::value, "lalg::dot(Vec<L> lhs, Vec<R> rhs): lhs and rhs must be vectors of the same underlying type.");
@@ -304,11 +305,13 @@ namespace mbl {
                 return acc;
             }
 
+            /** Return the magnitude of `VecLike v` */
             template <VecLike V>
             float magnitude(const V& v) {
                 return std::sqrt((float) dot(v, v));
             }
 
+            /** Return the cosine of `VecLike v` and `VecLike w`. */
             template <VecLike L, VecLike R>
             float cosine_of(const L& v, const R& w) {
                 static_assert(VecSize<L> == VecSize<R>, "lalg::cosine_of(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
@@ -332,11 +335,13 @@ namespace mbl {
 
             /** HYBRID LAZY + EAGER */
 
+            /** Returns a `VecLike` that is the input `VecLike v` but normalized. */
             template <VecLike V>
             DivVec<V,float> unit(const V& v) {
                 return v / magnitude(v);
             }
 
+            /** Produce a new `VecLike` that is the project of `VecLike v` onto `VecLike onto`. */
             template <VecLike L, VecLike R>
             Vec<VecValue<L>,VecSize<L>> project_onto(const L& v, const R& onto) {
                 static_assert(std::is_same<VecValue<L>, VecValue<R>>::value, "lalg::project_onto(Vec<L> lhs, Vec<R> rhs): lhs and rhs must be vectors of the same underlying type.");
@@ -344,6 +349,7 @@ namespace mbl {
                 return dot(v, b_unit) * b_unit;
             }
 
+            /** Produce a new `VecLike` that is the cross product of `l` and `r`. */
             template <VecLike L, VecLike R>
             Vec<OpResult<std::multiplies<>, VecValue<L>, VecValue<R>>,3> cross(const L& v, const R& w) {
                 using T = OpResult<std::multiplies<>, VecValue<L>, VecValue<R>>;
@@ -356,6 +362,7 @@ namespace mbl {
                 + ((v[0] * w[1] - v[1] * w[0]) * k);
             }
 
+            /** Produce a new `VecLike` where each element i is the maximum of `l[i]` and `r[i]`. */
             template <VecLike L, VecLike R>
             auto max(const L& l, const R& r) {
                 static_assert(std::is_same<VecValue<L>, VecValue<R>>::value, "lalg::max(Vec<L> lhs, Vec<R> rhs): lhs and rhs must be vectors of the same underlying type.");
@@ -364,6 +371,7 @@ namespace mbl {
                 return mapply(l, r, [](const T& a, const T& b) -> T { return std::max(a,b); });
             }
 
+            /** Produce a new `VecLike` where each element i is the minimum of `l[i]` and `r[i]`. */
             template <VecLike L, VecLike R>
             auto min(const L& l, const R& r) {
                 static_assert(std::is_same<VecValue<L>, VecValue<R>>::value, "lalg::min(Vec<L> lhs, Vec<R> rhs): lhs and rhs must be vectors of the same underlying type.");
@@ -372,6 +380,7 @@ namespace mbl {
                 return mapply(l, r, [](const T& a, const T& b) -> T { return std::min(a,b); });
             }
 
+            /** Produce a new `VecLike` that is the elementwise product of input `VecLike L` and `VecLike R`. */
             template <VecLike L, VecLike R>
             auto elementwise(const L& l, const R& r) {
                 static_assert(std::is_same<VecValue<L>, VecValue<R>>::value, "lalg::min(Vec<L> lhs, Vec<R> rhs): lhs and rhs must be vectors of the same underlying type.");
@@ -387,6 +396,47 @@ namespace mbl {
                     accumulator = f(accumulator, v[i]);
                 }
                 return accumulator;
+            }
+
+            /** Compute the squared distance between two `VecLike`s. */
+            template <VecLike L, VecLike R> requires std::is_default_constructible_v<VecValue<L>> && std::is_same_v<VecValue<L>,VecValue<R>>
+            VecValue<L> distance_squared(const L& l, const R& r) {
+                using T = VecValue<L>;
+                T acc = {};
+                for (size_t i = 0; i < VecSize<L>; i++) {
+                    T diff = l[i] - r[i];
+                    acc = acc + (diff * diff);
+                }
+                return acc;
+            }
+
+            /** Compute the distance between two `VecLike`s. */
+            template <VecLike L, VecLike R> requires std::is_default_constructible_v<VecValue<L>> && std::is_same_v<VecValue<L>,VecValue<R>>
+            float distance(const L& l, const R& r) {
+                return std::sqrtf((float) distance_squared(l,r));
+            }
+
+            /** Clamp all values in some `VecLike` between low and high thresholds. */
+            template <VecLike V>
+            auto clamp(const V& v, VecValue<V> low, VecValue<V> high) {
+                return map(v, [low,high](VecValue<V> i){
+                    if (i < low) return low;
+                    if (i > high) return high;
+                    return i;
+                });
+            }
+
+            /** Check whether VecLike  */
+            template <VecLike L, VecLike R> requires std::is_same_v<VecValue<L>,VecValue<R>> && std::is_floating_point_v<VecValue<L>>
+            bool equal_eps(const L& l, const R& r, VecValue<L> eps = 1e-3f) {
+                static_assert(VecSize<L> == VecSize<R>, "lalg::cosine_of(Vec<L> lhs, Vec<R> rhs): lhs and rhs must have the same size.");
+
+                for (size_t i = 0; i < VecSize<L>; i++) {
+                    if (std::abs(l[i] - r[i]) > eps) {
+                        return false;
+                    }
+                }
+                return true;
             }
 
             using Vec3 = Vec<float,3>;
