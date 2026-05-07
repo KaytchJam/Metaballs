467963141a31a297bfebd4259b774bd6cd0dd985
KJam <kaytchjam@gmail.com>
2026-05-07 10:57:08 -0500

    added tests for MetaballWorkingSet

--- src/goutte/tests/surface_test.cpp
+++ src/goutte/tests/surface_test.cpp
@@ -12,6 +12,7 @@
 #include <fieldrange.hpp>
 #include <aabb_tree.hpp>
 #include <metaball_presets.hpp>
+#include <metaball_working_set.hpp>
 
 // STDLIB
 #include <iostream>
@@ -19,6 +20,8 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <sstream>
+#include <ranges>
+#include <span>
 
 typedef int32_t uf_index;
 
@@ -312,6 +315,24 @@ int naive_2() {
     return EXIT_SUCCESS;
 }
 
+template <typename>
+struct FnHelper;
+
+template <typename Ret, typename... Args>
+struct FnHelper<Ret(Args...)> {
+    using type = Ret(*)(Args...);
+};
+
+template <typename Signature>
+using Fn = typename FnHelper<Signature>::type;
+
+template <typename T>
+std::string to_string(const gtt::lalg::vec<T,3>& v) {
+    std::stringstream ss;
+    ss << "(" << v.x << "," << v.y << "," << v.z << ")";
+    return ss.str();
+}
+
 /** Implementation 3 of coalescing overlapping bounding boxes in an AABB Tree. This 3rd implementation
  * uses a Disjoint Set Union. */
 int naive_3() {
@@ -321,13 +342,14 @@ int naive_3() {
         KB(gtt::lalg::vec3(1.f)),
         KB(gtt::lalg::vec3(-2.f)),
         KB(gtt::lalg::vec3(-5.f)),
-        KB(gtt::lalg::vec3(0, 4, -4), gtt::lalg::vec3(0), 2.f)
+        KB(gtt::lalg::vec3(0, 4, -4), gtt::lalg::vec3(0), 2.f),
+        KB(gtt::lalg::vec3(0, 5, -4)),
+        KB(gtt::lalg::vec3(6, 2, 4))
     };
 
     const size_t num_metaballs = sizeof(blobs) / sizeof(KB);
 
-    std::cout << "Insert into AABBTree" << std::endl;
-    // Insert into the AABBTree
+    // Insert all nodes into the AABBTree
     gtt::AABBTree tree;
     int i = 0;
     for (KB& blob : blobs) {
@@ -340,62 +362,180 @@ int naive_3() {
         uf.unite(tree.nodes[a].data_index, tree.nodes[b].data_index);
     });
 
-    std::cout << "{" << std::endl;
-    for (int i : IntRange(0, uf.num_nodes())) {
-        std::cout << "\t" << i << " : " << uf.find(i) << std::endl;
+    typedef int32_t mball_idx_t;
+
+    struct MetaballRenderingGroup {
+        BoundingBox bb = BoundingBox::empty();
+        std::span<const mball_idx_t> indices;
+    };
+
+    auto collector = gtt::dsa::UnionFindCollector(uf);
+    std::vector<MetaballRenderingGroup> groups;
+
+    std::cout << "Populating Groups" << std::endl;
+    for (auto component : collector.components()) {
+        std::cout << "COMPONENT = (" << component.start << ", " << component.end << ")" << std::endl;
+        MetaballRenderingGroup mre;
+        mre.indices = std::span(collector.get_accessor().sorted_mappings.data() + component.start, (size_t) component.end - component.start );
+        for (int i = component.start; i < component.end; i++) {
+            mre.bb = gtt::join(mre.bb, blobs[i].get_bounding_box());
+        }
+
+        groups.push_back(mre);
     }
-    std::cout << "}" << std::endl;
 
-    std::cout << "PRINTING OUT COMPONENTS: " << std::endl;
+    typedef gtt::IsoSurface IS;
+    typedef BoundingBox BB;
+    typedef FieldRange FR;
 
-    for (auto group : gtt::dsa::UnionFindCollector(uf).components()) {
-        std::cout << "SET = [ ";
-        for (int i = group.start; i < group.end; i++) {
-            std::cout << i << " ";
+    gtt::IsoSurface is = gtt::IsoSurface::construct(gtt::common::lalg::vec3(0), 10.f, 5);
+
+    const Fn<FR(const IS&, const BB&)> box_to_fieldrange = [](const IS& surface, const BB& bb) {
+        const gtt::lalg::ivec3 start = floor(surface.position_to_index(bb.min_point));
+        const gtt::lalg::ivec3 end = ceil(surface.position_to_index(bb.max_point));
+        return FieldRange({start.x, end.x, start.y, end.y, start.z, end.z});
+    };
+
+    std::cout << "Getting Field Ranges" << std::endl;
+    int k = 0;
+    for (MetaballRenderingGroup& mrg : groups) {
+        FieldRange field = box_to_fieldrange(is, mrg.bb);
+
+        std::cout << "BOX #" << k << " = " << to_string(mrg.bb) << " {" << std::endl;
+        for (auto idx : field) {
+            std::cout << "\t" << to_string(idx) << " = " << to_string(is.get(idx.x, idx.y, idx.z).position) << std::endl;
+        }
+        std::cout << "}" << std::endl;
+        k += 1;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int naive_4() {
+    using namespace gtt;
+    using namespace presets;
+    using KB = KineticBlob;
+
+    KB blobs[] = {
+        KB(),
+        KB(gtt::lalg::vec3(1.f)),
+        KB(gtt::lalg::vec3(-2.f)),
+        KB(gtt::lalg::vec3(-5.f)),
+        KB(gtt::lalg::vec3(0, 4, -4), gtt::lalg::vec3(0), 2.f),
+        KB(gtt::lalg::vec3(0, 5, -4)),
+        KB(gtt::lalg::vec3(6, 2, 4))
+    };
+    
+    gtt::IsoSurface is = gtt::IsoSurface::construct(gtt::common::lalg::vec3(0), 10.f, 5);
+
+    std::cout << "WorkingSet constructor" << std::endl;
+    gtt::MetaballWorkingSet<KineticBlob> metaballs(is);
+
+    std::cout << "Inserting metaballs into working set" << std::endl;
+    for (KB& kb : blobs) {
+        metaballs.add_metaball(std::move(kb));
+    }
+
+    std::cout << "Iterating through the groups" << std::endl;
+    int group_number = 0;
+    for (MetaballWorkingSet<KB>::RenderGroup i : metaballs.groups()) {
+        std::cout << "group_number : " << group_number << " = [ ";
+        for (int index : i.ball_indices) {
+            std::cout << index << " ";
         }
         std::cout << "]" << std::endl;
+        group_number += 1;
     }
 
-    // component_number prev_comp = -1;
-    // int32_t last_index = -1;
-    // BoundingBox bb;
+    return EXIT_SUCCESS;
+}
 
-    // for (int i = 0; i < ufc.size(); i++) {
-    //     const metaball_index m = ufc.flat[i];
-    //     const component_number c = ufc.counts[m];
+int naive_5() {
+    using namespace gtt;
+    using namespace presets;
+    using BALL = InverseSquareCube;
 
-    //     if (c != prev_comp || prev_comp == -1) {
-    //         if (last_index != -1) {
-    //             std::cout << to_string(bb) << ", [ ";
-    //             for (metaball_index m : IntRange(last_index, i)) { std::cout << m << " "; }
-    //             std::cout << "], Component = " << ufc.counts[ufc.flat[last_index]] << std::endl;
-    //         }
+    BALL blobs[] = {
+        BALL(),
+        BALL(gtt::lalg::vec3(1.f)),
+        BALL(gtt::lalg::vec3(-2.f)),
+        BALL(gtt::lalg::vec3(-5.f)),
+        BALL(gtt::lalg::vec3(0, 4, -4)),
+        BALL(gtt::lalg::vec3(0, 5, -4)),
+        BALL(gtt::lalg::vec3(6, 2, 4))
+    };
+    
+    gtt::IsoSurface is = gtt::IsoSurface::construct(gtt::common::lalg::vec3(0), 10.f, 5);
 
-    //         last_index = i;
-    //         bb = blobs[m].get_bounding_box();
-    //         prev_comp = c;
-    //     } else {
-    //         bb = gtt::join(bb, blobs[m].get_bounding_box());
-    //     }
-    // }
+    std::cout << "WorkingSet constructor" << std::endl;
+    gtt::MetaballWorkingSet<BALL> metaballs(is);
 
-    // if (ufc.size() > 0) {
-    //     std::cout << to_string(bb) << " : [ ";
-    //     for (metaball_index m : IntRange(last_index, (int32_t) ufc.size())) { std::cout << m << " "; }
-    //     std::cout << "], Component = " << ufc.counts[ufc.flat[ufc.size() - 1]] << std::endl;
-    // }
+    std::cout << "Inserting metaballs into working set" << std::endl;
+    for (BALL& b : blobs) {
+        metaballs.add_metaball(std::move(b));
+    }
+
+    std::cout << "Iterating through the groups" << std::endl;
+    int group_number = 0;
+    for (MetaballWorkingSet<BALL>::RenderGroup i : metaballs.groups()) {
+        std::cout << "group_number : " << group_number << " = [ ";
+        for (int index : i.ball_indices) {
+            std::cout << index << " ";
+        }
+        std::cout << "] ";
+
+        FieldRange& fr = i.fr;
+        std::cout << "START = " << to_string(fr.low()) << ", END = " << to_string(fr.high()) << std::endl;
+        group_number += 1;
+    }
 
     return EXIT_SUCCESS;
 }
 
 int main() {
     using naive_test = int (*)();
-    naive_test tests[] = { naive_1, naive_2, naive_3 };
-    constexpr int test_index = 2;
+    naive_test tests[] = { naive_1, naive_2, naive_3, naive_4, naive_5 };
+    constexpr int test_index = 4;
 
-    std::cout << "Executing test 'naive_" << (test_index + 1) << "'!" << std::endl;
-    const int result = tests[test_index]();
-    std::cout << "Test ended with status = " << result << std::endl;
+    constexpr int NUM_TESTS = sizeof(tests) / sizeof(naive_test);
+
+    while (true) {
+        bool valid_input = false;
+        std::string input;
+        int test_num = -1;
+
+        while (!valid_input) {
+            std::cout << "\n\nEnter a test number in the range 1-" << NUM_TESTS << ": ";
+            std::cin >> input;
+            std::cout << "\n";
+
+            std::string_view view(input.data() + input.find_first_not_of(" \t\n!@#$%^&*()-_+=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\\|/{}[]:;\"'<>,.?~`"), 1);
+
+            if (input == "q") {
+                std::cout << "Closing program." << std::endl;
+                return EXIT_SUCCESS;
+            }
+
+            try {
+                test_num = std::abs(std::stoi(input));
+                if (test_num > NUM_TESTS || test_num <= 0) {
+                    std::cout << "Inputted test is outside range. Please enter again... or enter 'q' to quit." << std::endl;
+                } else {
+                    valid_input = true;
+                }
+            } catch (const std::invalid_argument& e) {
+                std::cout << "Error: " << e.what() << std::endl;
+                std::cout << "Invalid input. Please enter again... or enter 'q' to quit." << std::endl;
+            }
+        }
+
+        std::cout << "\n=================================" << std::endl;
+        std::cout << "Executing test 'naive_" << (test_index + 1) << "'!\n" << std::endl;
+        const int result = tests[test_num - 1]();
+        std::cout << "Test ended with status = " << result << std::endl;
+        std::cout << "\n=================================" << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
