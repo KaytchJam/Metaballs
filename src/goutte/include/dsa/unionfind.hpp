42dfa549c0762d618403a5fbae1c27f99ab8c3d6
KJam <kaytchjam@gmail.com>
2026-06-02 20:05:32 -0500

    reset_remove and pop_unsafe

--- src/goutte/include/dsa/unionfind.hpp
+++ src/goutte/include/dsa/unionfind.hpp
@@ -30,8 +30,12 @@ namespace gtt {
                 uf_index_t find(const uf_index_t x) const;                  // Find the representative node of the set containing `x`.
                 UnionFind& unite(const uf_index_t a, const uf_index_t b);   // Join the sets containing nodes `a` and `b`.
                 UnionFind& add_vertex();                                    // Adds a new set index 'N' to a UnionFind of size N
+                UnionFind& remove_vertex(const uf_index_t a);               // Removes vertex at index a via swap & pop. 
+                UnionFind& reset_remove();                                  // Reset & pop back the UnionFind 
                 UnionFind& reset();                                         // Reset the UnionFind sets to all be disjoint
                 UnionFind& isolate(const uf_index_t a);                     // Make node `a` disjoint. 
+                UnionFind& update(const uf_index_t a, const uf_index_t b);  // Update all nodes n with parent(n) = a to parent(n) = b
+                UnionFind& pop_unsafe();                                    // Reduce the size of the UnionFind and make no other modifications
 
                 inline bool is_root(const uf_index_t x) const;              // Returns whether node `x` is a root node or not.
                 inline size_t num_nodes() const;                            // Returns the total number of nodes in this union find struct.
